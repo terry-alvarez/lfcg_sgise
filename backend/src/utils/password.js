6722aafbd2b568ab6ff7c056a51253ecd4933b0b
terry-alvarez <halvarez.lfcg@gmail.com>
2026-05-28 13:36:03 -0500

    feat: Add password utility functions

---
+++ backend/src/utils/password.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcryptjs';
+
+const SALT_ROUNDS = 10;
+
+export const hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    return await bcrypt.hash(password, salt);
+  } catch (error) {
+    throw new Error('Error hashing password');
+  }
+};
+
+export const comparePassword = async (password, hash) => {
+  try {
+    return await bcrypt.compare(password, hash);
+  } catch (error) {
+    throw new Error('Error comparing password');
+  }
+};
